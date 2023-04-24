49cba64d44ac52888807cba3a84b246695fa15c3
Alex42 <alexcrazy42@mail.ru>
2023-04-24 23:22:57 +0300

    learn main commands of SQL in EF

--- EntityFrameworkLearn/Program.cs
+++ EntityFrameworkLearn/Program.cs
@@ -1,27 +1,53 @@
 ﻿using EntityFrameworkLearn.Data;
 using EntityFrameworkLearn.Models;
-
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 var db = new UserContext();
 
+// SELECT
+var users = db.Users
+    .Where(u => u.UserId >= 17).ToList()
+    .Where(u => u.UserNickname[0] == 'a');
+foreach (var u in users)
+{
+    Console.WriteLine($"{u.UserId} : {u.UserNickname}");
+}
 
-db.Users.AddRange(
-    new User { UserNickname = "alexcrazy42"}, 
-    new User { UserNickname = "alexcrazy43" }
-);
-db.SaveChanges();
 
-var user = db.Users.Where(u => u.UserId == 10).Single();
+// SELECT DISTINCT
+var usersNames = db.Users
+    .Where(u => u.UserId > 17)
+    .Select(u => u.UserNickname)
+    .Distinct().ToList();
+foreach (var userName in usersNames)
+{
+    Console.WriteLine($"{userName}");
+}
+
+//// INSERT
+//db.Users.AddRange(
+//    new User { UserNickname = "alexcrazy42" },
+//    new User { UserNickname = "alexcrazy43" }
+//);
+//db.SaveChanges();
+
+
+
+// UPDATE
+var user = db.Users.Where(u => u.UserId == 20).Single();
 
 if (user != null)
 {
-    Console.WriteLine(user.UserNickname);
+    user.UserNickname = "Привет";
+    db.SaveChanges();
+
 }
 else
 {
     Console.WriteLine("Такого пользователя нет");
 }
 
+// DELETE
 var usersToDelete = db.Users.ToList();
 db.Users.RemoveRange(usersToDelete);
 db.SaveChanges();
