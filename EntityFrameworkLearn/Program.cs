193c8dda3662ea36e23b9d2077ecac4ee2f7774d
Alex42 <alexcrazy42@mail.ru>
2023-04-28 23:38:17 +0300

    stop at inner join

--- EntityFrameworkLearn/Program.cs
+++ EntityFrameworkLearn/Program.cs
@@ -1,7 +1,8 @@
 ﻿using EntityFrameworkLearn.Data;
-using EntityFrameworkLearn.Models;
-using Microsoft.EntityFrameworkCore.Internal;
-using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Infrastructure.Internal;
+using System.Diagnostics;
+using System.Linq;
 
 var db = new UserContext();
 
@@ -9,13 +10,15 @@ var db = new UserContext();
 
 ////// SELECT
 //var users = db.Users
-//    .Where(u => u.UserId >= 17).ToList()
-//    .Where(u => u.UserNickname[0] == 'a');
-//foreach (var u in users)
-//{
+//    .Where(u => u.UserId >= 17)
+//    .ToList();
 
-//    Console.WriteLine($"{u.UserId} : {u.UserNickname}");
-//}
+
+//var usersByLinq = from user in db.Users
+//                  where user.UserId >= 17
+//                  select user;
+
+
 
 
 //// SELECT DISTINCT
@@ -23,35 +26,52 @@ var db = new UserContext();
 //    .Where(u => u.UserId > 17)
 //    .Select(u => u.UserNickname)
 //    .Distinct().ToList();
-//foreach (var userName in usersNames)
-//{
-//    Console.WriteLine($"{userName}");
-//}
+
+
+//var userNamesByLinq = (from user in db.Users
+//                      where user.UserId > 17
+//                      select user.UserNickname).Distinct();
+
 
 //// SELECT MULTIPLY COLUMNS WITH 'OR' OPERATION ('AND')
 //var users = db.Users
 //    .Where(u => (u.UserId > 17 || u.UserNickname == "alex"))
 //    .Select(u => new { u.UserId, u.UserNickname })
 //    .ToList();
-//foreach (var user in users)
-//{
-//    Console.WriteLine($"{user.UserId} {user.UserNickname}");
-//}
+
+//var userByLinq = from user in db.Users
+//                 where user.UserId >= 17 || user.UserNickname == "alex"
+//                 select new { user.UserId, user.UserNickname };
+
 
 //// SUBSTRING
 //var users = db.Users
 //    .Select(u => u.UserNickname.Substring(0, u.UserNickname.Length))
 //    .ToList();
 
+//var usersByLinq = from user in db.Users
+//                  select user.UserNickname.Substring(0, user.UserNickname.Length);
+
+
 //// ORDER BY
 //var users = db.Users
 //    .OrderByDescending(u => u.UserId)
 //    .ToList();
 
+//var usersByLinq = from user in db.Users 
+//                  orderby user.UserId descending 
+//                  select user;
+
+
+
 //// GROUP BY
 //var users = db.Users
 //    .GroupBy(user => user.UserNickname)
-//    .Select(user => new { count = user.Count(), name = user.Key}).ToList();
+//    .Select(user => new { count = user.Count(), name = user.Key }).ToList();
+
+//var userByLinq = from user in db.Users
+//                 group user by user.UserNickname;
+
 
 
 //// BETWEEN
@@ -59,22 +79,33 @@ var db = new UserContext();
 //    .Where(u => (u.UserId >= 22 && u.UserId < 24))
 //    .ToList();
 
+//var usersByLinq = from user in db.Users
+//                  where user.UserId >= 22 && user.UserId < 24
+//                  select user;
+
+
+
 //// IN 
 //var users = db.Users.
 //    Where(u => new int[] { 1, 23, 4 }.Contains(u.UserId))
 //    .ToList();
 
+//var userByLinq = from u in db.Users
+//                 where new int[] { 1, 23, 4 }.Contains(u.UserId)
+//                 select u;
+
 
 //// NEATED SUBQUERIES ???
 //var users = db.Users
-//    .Where(u1 => 
+//    .Where(u1 =>
 //    (db.Users.Where(u => u.UserId < 25).Select(u => u.UserNickname).ToList().Contains(u1.UserNickname)))
 //    .ToList();
 
-//foreach (var user in users)
-//{
-//    Console.WriteLine($"{user.UserId} {user.UserNickname}");
-//}
+//var usersByLinq = from u in db.Users
+//                where (from u1 in db.Users
+//                        where u1.UserId < 25
+//                        select u1.UserNickname).Contains(u.Contains())
+
 
 
 //// INNER JOIN 
@@ -85,14 +116,12 @@ var db = new UserContext();
 //    (user, car) => new { User = user, Car = car })
 //    .ToList();
 
+//var userCarsByLinq = from user in db.Users
+//                     join car in db.Cars on user.UserId equals car.UserId
+//                     select new { User = user, Car = car };
 
 // LEFT JOIN 
 
-
-//foreach (var userCar in userCars)
-//{
-//    Console.WriteLine($"{userCar.User.UserId} {userCar.User.UserNickname} {userCar.Car.CarName}");
-//}
 
 
 //// INSERT
